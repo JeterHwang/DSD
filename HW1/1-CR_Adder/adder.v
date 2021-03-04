112767b2a393b06de9c38643074a2e6d1eb1cfd2
jeter <jeter@example.com>
2021-03-04 17:01:04 +0800

    adder gate level

--- HW1/1-CR_Adder/adder.v
+++ HW1/1-CR_Adder/adder.v
@@ -4,8 +4,8 @@ output carry;
 output [7:0] out;
 
 /*Write your code here*/
-
-
+assign out      = ({1'b0, x} + {1'b0, y})[7:0];
+assign carry    = ({1'b0, x} + {1'b0, y})[8];
 /*End of code*/
 
 endmodule
--- HW1/1-CR_Adder/adder_gate.v
+++ HW1/1-CR_Adder/adder_gate.v
@@ -4,8 +4,36 @@ output carry;
 output [7:0] out;
 
 /*Write your code here*/
+wire [8:0] C;
 
+assign C[0] = 1'b0;
+assign carry = C[8];
+
+generate;
+    genvar i;
+    for(i = 0; i < 8; i = i + 1) begin
+        FA full_adder(.Cin(C[i]), .a(x[i]), .b(y[i]), .Cout(C[i + 1]), .S(out[i]));
+    end
+endgenerate
 
 /*End of code*/
+
+endmodule
+
+module FA(Cin, a, b, Cout, S);
+    input Cin;
+    input a;
+    input b;
+    output Cout;
+    output S;
+    
+    wire aXb, aAb, w1;
+
+    xor2 XO1(.in1(a), .in2(b), .out(aXb));
+    xor2 XO2(.in1(aXb), .in2(Cin), .out(S));
+    and2 AN1(.in1(a), .in2(b), .out(aAb));
+    and2 AN2(.in1(aXb), .in2(Cin), .out(w1));
+    or2  OR1(.in1(w1), .in2(aAb), .out(Cout));
+
 
 endmodule
