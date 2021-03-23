b8f330f5c6c53c2208899df960c9028ade5c618f
JeterHwang <jeter0309@gmail.com>
2021-03-23 23:01:47 +0800

    update

--- HW2/2-RegisterFile/register_file.v
+++ HW2/2-RegisterFile/register_file.v
@@ -18,14 +18,27 @@ output [7:0] busX, busY;
 reg [7:0] r0_w, r1_w, r2_w, r3_w, r4_w, r5_w, r6_w, r7_w;
 reg [7:0] r0_r, r1_r, r2_r, r3_r, r4_r, r5_r, r6_r, r7_r;
 
-reg [7:0] data_x_r, data_x_w;
-reg [7:0] data_y_r, data_y_w;
 
-assign busX = data_x_r;
-assign busY = data_y_r;
+
+assign busX = RX == 3'b000 ? r0_r
+            : RX == 3'b001 ? r1_r
+            : RX == 3'b010 ? r2_r
+            : RX == 3'b011 ? r3_r
+            : RX == 3'b100 ? r4_r
+            : RX == 3'b101 ? r5_r
+            : RX == 3'b110 ? r6_r
+            : r7_r;
+assign busY = RY == 3'b000 ? r0_r
+            : RY == 3'b001 ? r1_r
+            : RY == 3'b010 ? r2_r
+            : RY == 3'b011 ? r3_r
+            : RY == 3'b100 ? r4_r
+            : RY == 3'b101 ? r5_r
+            : RY == 3'b110 ? r6_r
+            : r7_r;
     
 always@(*) begin
-    r0_w        = 8'd0;
+    r0_w        = r0_r;
     r1_w        = r1_r;
     r2_w        = r2_r;
     r3_w        = r3_r;
@@ -33,8 +46,6 @@ always@(*) begin
     r5_w        = r5_r;
     r6_w        = r6_r;
     r7_w        = r7_r;
-    data_x_w    = data_x_r;
-    data_y_w    = data_y_r;
     if(WEN) begin
         case (RW)
             3'b001: r1_w = busW;
@@ -46,32 +57,10 @@ always@(*) begin
             3'b111: r7_w = busW;
         endcase
     end
-    else begin
-        case (RX)
-            3'b000: data_x_w = r0_r;
-            3'b001: data_x_w = r1_r;
-            3'b010: data_x_w = r2_r;
-            3'b011: data_x_w = r3_r;
-            3'b100: data_x_w = r4_r;
-            3'b101: data_x_w = r5_r;
-            3'b110: data_x_w = r6_r;
-            3'b111: data_x_w = r7_r;
-        endcase
-        case (RY)
-            3'b000: data_y_w = r0_r;
-            3'b001: data_y_w = r1_r;
-            3'b010: data_y_w = r2_r;
-            3'b011: data_y_w = r3_r;
-            3'b100: data_y_w = r4_r;
-            3'b101: data_y_w = r5_r;
-            3'b110: data_y_w = r6_r;
-            3'b111: data_y_w = r7_r;
-        endcase
-    end
 end
 
 always@(posedge Clk) begin
-    r0_r        <= r0_w;
+    r0_r        <= 8'd0;
     r1_r        <= r1_w;
     r2_r        <= r2_w;
     r3_r        <= r3_w;
@@ -79,8 +68,6 @@ always@(posedge Clk) begin
     r5_r        <= r5_w;
     r6_r        <= r6_w;
     r7_r        <= r7_w;
-    data_x_r    <= data_x_w;
-    data_y_r    <= data_y_w;
 end	
 
 endmodule
--- HW2/2-RegisterFile/register_file_tb.v
+++ HW2/2-RegisterFile/register_file_tb.v
@@ -20,8 +20,140 @@ module register_file_tb;
         busX ,
         busY
     );
-
+    initial Clk = 0;
+    always #(`HCYCLE) Clk = ~Clk; 
     // write your test pattern here
 
+    initial begin
+        $fsdbDumpfile("register_file.fsdb");
+        $fsdbDumpvars;
+    end
+
+    initial begin
+        @(negedge Clk) WEN = 1;
+        @(posedge Clk) begin
+            RW      = 3'b000;
+            busW    = 8'b00000000; 
+            
+            RX      = 3'b000;
+            RY      = 3'b001;
+            
+        end
+        @(posedge Clk) begin
+            RW      = 3'b001;
+            busW    = 8'b00000001;
+        end
+        @(posedge Clk) begin
+            RW      = 3'b010;
+            busW    = 8'b00000010;
+
+            $display("########### Output ###########");
+            if(busX != 8'b00000000)                
+                $display("busX Expect 8'b00000000 but %8b received !!", busX);
+            else
+                $display("busX pattern Pass !!");
+            if(busY != 8'b00000001)
+                $display("busY Expect 8'b00000001 but %8b received !!", busY);
+            else
+                $display("busY pattern Pass !!");
+            $display("##############################");
+        end
+        @(posedge Clk) begin
+            RW      = 3'b011;
+            busW    = 8'b00000011;
+
+            $display("########### Output ###########");
+            if(busX != 8'b00000000)                
+                $display("busX Expect 8'b00000000 but %8b received !!", busX);
+            else
+                $display("busX pattern Pass !!");
+            if(busY != 8'b00000001)
+                $display("busY Expect 8'b00000001 but %8b received !!", busY);
+            else
+                $display("busY pattern Pass !!");
+            $display("##############################");
+        end
+        @(posedge Clk) begin
+            RW      = 3'b100;
+            busW    = 8'b00000100;
+        end
+        @(posedge Clk) begin
+            RW      = 3'b101;
+            busW    = 8'b00000101;
+
+            RX      = 3'b010;
+            RY      = 3'b101;
+        end
+        @(posedge Clk) begin
+            RW      = 3'b110;
+            busW    = 8'b00000110;
+
+            $display("########### Output ###########");
+            if(busX != 8'b00000010)                
+                $display("busX Expect 8'b00000010 but %8b received !!", busX);
+            else
+                $display("busX pattern Pass !!");
+            if(busY != 8'b00000101)
+                $display("busY Expect 8'b00000101 but %8b received !!", busY);
+            else
+                $display("busY pattern Pass !!");
+            $display("##############################");
+        end
+        @(posedge Clk) begin
+            RW      = 3'b111;
+            busW    = 8'b00000111;
+
+            $display("########### Output ###########");
+            if(busX != 8'b00000010)                
+                $display("busX Expect 8'b00000010 but %8b received !!", busX);
+            else
+                $display("busX pattern Pass !!");
+            if(busY != 8'b00000101)
+                $display("busY Expect 8'b00000101 but %8b received !!", busY);
+            else
+                $display("busY pattern Pass !!");
+            $display("##############################");
+        end
+        @(negedge Clk) WEN = 0;
+        
+        RX = 3'b111;
+        RY = 3'b000;
+        #(`CYCLE * 3)
+        @(posedge Clk) begin
+            $display("########### Output ###########");
+            if(busX != 8'b00000111)                
+                $display("busX Expect 8'b00000111 but %8b received !!", busX);
+            else
+                $display("busX pattern Pass !!");
+            if(busY != 8'b00000000)
+                $display("busY Expect 8'b00000000 but %8b received !!", busY);
+            else
+                $display("busY pattern Pass !!");
+            $display("##############################");
+
+        end
+        @(posedge Clk) begin
+            WEN = 1;
+            RW  = 3'b000;
+            busW = 8'b00000111;
+
+            RX = 3'b000;
+            RY = 3'b001;
+        end
+        @(posedge Clk) begin
+            $display("########### Output ###########");
+            if(busX != 8'b00000000)                
+                $display("busX Expect 8'b00000000 but %8b received !!", busX);
+            else
+                $display("busX pattern Pass !!");
+            if(busY != 8'b00000001)
+                $display("busY Expect 8'b00000001 but %8b received !!", busY);
+            else
+                $display("busY pattern Pass !!");
+            $display("##############################");
+        end
+        $display("------ Simualation finish ------");
+        $finish;
+    end
 
 endmodule
