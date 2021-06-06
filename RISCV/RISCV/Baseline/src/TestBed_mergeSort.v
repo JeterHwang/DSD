e8e7c07221c6807b16fd34e0f2a11b96fda0f182
JeterHwang <jeter0309@gmail.com>
2021-06-06 12:10:48 +0800

    update

--- RISCV/RISCV/Baseline/src/TestBed_mergeSort.v
+++ RISCV/RISCV/Baseline/src/TestBed_mergeSort.v
@@ -1,8 +1,8 @@
 `timescale 1 ns/10 ps
-`define EndAddr     32'd255	 // 536 / 4
+`define EndAddr     30'd255	 // 536 / 4
 `define MemSize     8       // 8 array numbers
-`define Arrbegin    128
-`define Arrend      136
+`define Arrbegin    30'd128
+`define Arrend      30'd136
 `define golden     "./pattern/sortedArray.dat"
 
 module	TestBed(
@@ -28,13 +28,18 @@ module	TestBed(
 	reg		[15:0]	duration;
 	reg				finish;
 
-	reg     [1:0]   curstate;
+	reg     [1:0]   curstate, nxtstate;
 
 	reg signed [31:0] sortedArr  [0 : `MemSize - 1];
-	reg	signed [31:0] mem_answer [0:255];
+	reg	signed [31:0] mem_answer_r [0:255];
+	reg	signed [31:0] mem_answer_w [0:255];
 
 	wire    [31:0]  data_modify;
-		
+	
+	parameter	state_idle 	= 2'b00;
+	parameter	state_check = 2'b01;
+	parameter	state_report= 2'b10;
+	
 	integer i;
 
 	assign data_modify = {data[7:0],data[15:8],data[23:16],data[31:24]}; // convert little-endian format to readable format
@@ -42,19 +47,49 @@ module	TestBed(
 	initial begin
 		error_num 	= 0;
 		duration 	= 0;
-		finish 		= 0;
-		curstate 	= 2'b00;
 		$readmemh(`golden, sortedArr);
 	end
 
-	always@(posedge wen) begin
-		if(addr == `EndAddr) begin
+	always@(*) begin
+		for(i = 0; i < 256; i = i + 1)
+			mem_answer_w[i] = mem_answer_r[i];
+		case (curstate)
+			state_check: begin
+				finish = 1'b0;
+				if(wen) begin
+					//$display("Write %d on address %d", data_modify, addr);
+					if(addr == `EndAddr) begin
+						nxtstate = state_report;
+					end
+					else begin
+						nxtstate = state_check;
+					end
+					mem_answer_w[addr] 	= data_modify;
+				end
+				else begin
+					nxtstate = curstate;
+				end
+			end
+			state_report: begin
+				finish = 1'b1;
+				nxtstate = state_report;
+			end
+			default: begin
+				finish = 1'b0;
+				nxtstate = curstate;
+			end
+		endcase
+	end
+
+	always@( negedge clk )						
+	begin
+		if(curstate == state_report) begin
 			$display("--------------------------- Simulation FINISH !!---------------------------");
 			for(i = `Arrbegin; i < `Arrend; i = i + 1) begin
-				if(mem_answer[i] !== sortedArr[i - `Arrbegin]) begin
+				if(mem_answer_r[i] !== sortedArr[i - `Arrbegin]) begin
 					if(i == 0)
 						$display("Error !!\n");
-					$display("Element #%d : expected %d while got %d\n", i - `Arrbegin + 1, sortedArr[i - `Arrbegin], mem_answer[i]);
+					$display("Element #%d : expected %d while got %d\n", i - `Arrbegin + 1, sortedArr[i - `Arrbegin], mem_answer_r[i]);
 					error_num = error_num + 1;
 				end
 			end
@@ -68,12 +103,22 @@ module	TestBed(
 				$display("\n \\(^o^)/ CONGRATULATIONS!!  The simulation result is PASS!!!\n");
 				$display("============================================================================");
 			end
-			curstate 	= 2'b10;
-			finish 		= 1;
 		end
-		else begin
-			mem_answer[addr] 	= data_modify;
-			curstate 			= 2'b01;
+	end
+
+	always@( posedge clk or negedge rst )						// State-DFF
+	begin
+		if( ~rst )
+		begin
+			curstate <= state_check;
+			for(i = 0; i < 256; i = i + 1)
+				mem_answer_r[i] <= 32'd0;
+		end
+		else
+		begin
+			curstate <= nxtstate;
+			for(i = 0; i < 256; i = i + 1)
+				mem_answer_r[i] <= mem_answer_w[i];
 		end
 	end
 endmodule
